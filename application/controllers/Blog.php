7a1e1965c7a569eb8f8f25da27049039552ac4eb
juanoude <juanoude@gmail.com>
2018-10-21 14:25:53 -0300

    criando a postagem do blog

---
+++ application/controllers/Blog.php
@@ -0,0 +1,20 @@
+<?php
+  defined('BASEPATH') OR exit('No direct script access allowed');
+
+  class Blog extends CI_Controller{
+    public function index(){
+      $cssespecifico = "blog.css";
+      $dados = [
+        'cssespecifico' => $cssespecifico
+      ];
+      $this->load->view("blog/index", $dados);
+    }
+
+    public function novo_post(){
+      $cssespecifico = "blog.css";
+      $dados = [
+        'cssespecifico' => $cssespecifico
+      ];
+      $this->load->view("blog/novo_post", $dados);
+    }
+  }
